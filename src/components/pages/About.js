823852f5d10291490621fa73e04ee5898ba8449f
Andrea <andbruna@localhost.localdomain>
2018-09-27 22:03:09 +0200

    initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4">Contact Manager</h1>
+      <p className="lead"> Simple app to manage contacts</p>
+      <p className="text-secondary"> V1.0</p>
+    </div>
+  );
+};
